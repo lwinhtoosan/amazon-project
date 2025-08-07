abefffcb7663b435381fb4b898168ae74ef75466
lwinhtoosan <lwinhtoosan2001@gmail.com>
2025-08-07 23:06:36 +0630

    made interactive add to button and cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -34,10 +34,41 @@ products.forEach((product) => {
                         <img class="added-to-cart-img" src="/images/checkmark.png">
                         Added
                     </div>
-                    <button class="add-to-cart-btn button-primary">Add to Cart</button>
+                    <button class="add-to-cart-btn button-primary js-add-to-cart-btn"
+                    data-product-id="${product.id}">Add to Cart</button>
                 </div>`;
 }
 )
-console.log(productHTML)
 document.querySelector('.product-grid')
 .innerHTML = productHTML;
+
+document.querySelectorAll('.js-add-to-cart-btn')
+.forEach((button) => {
+    button.addEventListener('click', () => {
+        let productId =  button.dataset.productId;
+
+        let matchingItem;
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        })
+        if (matchingItem) {
+            matchingItem.quantity +=1;
+            
+        } else {
+            cart.push( {
+            productId: productId,
+            quantity: 1
+        })
+        }
+        
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+          cartQuantity  +=item.quantity
+        })
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+        console.log(cart)
+    })
+})
