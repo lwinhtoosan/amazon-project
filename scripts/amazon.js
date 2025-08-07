fe7aa9e6a10e0e51aae9a0c6cb61ec05c977d45d
lwinhtoosan <lwinhtoosan2001@gmail.com>
2025-08-07 21:29:44 +0630

    used-products.js

---
+++ scripts/amazon.js
@@ -0,0 +1,43 @@
+
+let productHTML = "";
+products.forEach((product) => {
+    productHTML += `
+    <div class="product-container">
+                    <div class="product-image-container">
+                        <img class="product-image"
+                             src='${product.image}'>
+                    </div>
+                    <div class="product-name limit-text-to-2-lines ">
+                        ${product.name}
+                    </div>
+                    <div class="product-rate-container">
+                        <img class="product-rating-star" src="/images/ratings/rating-${product.rating.stars*10}.png">
+                        <div class="product-rating-point">${product.rating.count}</div>
+                    </div>
+                    <div class="price-text">$${(product.priceCents / 100).toFixed(2)}</div>
+                    <div class="product-quantity-container">
+                        <select>
+                            <option selected value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </div>
+                    <div class="product-spacer"></div>
+                    <div class="added-to-cart">
+                        <img class="added-to-cart-img" src="/images/checkmark.png">
+                        Added
+                    </div>
+                    <button class="add-to-cart-btn button-primary">Add to Cart</button>
+                </div>`;
+}
+)
+console.log(productHTML)
+document.querySelector('.product-grid')
+.innerHTML = productHTML;
