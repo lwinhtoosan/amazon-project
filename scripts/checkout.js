86738f6dac44c72170494ca62040abef3480cc8e
lwinhtoosan <lwinhtoosan2001@gmail.com>
2025-08-09 21:21:07 +0630

    remove cartItem when clicki delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -29,7 +29,7 @@ cart.forEach((cartItem) => {
                         <div class="product-quantity">
                         <span>Quantity : <span class="quantity-label">${cartItem.quantity}</span></span>
                         <span class="update-quantity-link link-primary">Update</span>
-                        <span class="delete-quantity-link link-secondary">Delete</span>
+                        <span class="delete-quantity-link link-secondary js-delete-link" data-product-id=${matchingProduct.id}>Delete</span>
                         </div>
                     </div>
                     </div>
@@ -74,3 +74,12 @@ cart.forEach((cartItem) => {
 })
 document.querySelector('.js-order-summary')
  .innerHTML = cartSummaryHTML;
+
+ document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId
+        removeQuantity(productId)
+        console.log(cart)
+    })
+  })
