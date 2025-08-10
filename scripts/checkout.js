4a094df82cb05868c5a8f0eac8e215fc0eac10dd
lwinhtoosan <lwinhtoosan2001@gmail.com>
2025-08-10 21:18:14 +0630

    update cartQuantity when click each delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = '';
+updateCartQuantity();
 cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
@@ -84,12 +85,17 @@ document.querySelectorAll('.js-delete-link')
 
         const container =  document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove();
-    })
+        updateCartQuantity();
+    });
   })
 
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
+  
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity
   })
 document.querySelector('.js-return-to-home-link')
 .innerHTML = `${cartQuantity} items`
+}
+  
