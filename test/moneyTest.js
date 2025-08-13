ea8d7e7fb874921cb42adf58635a1826691ae7d4
lwinhtoosan <lwinhtoosan2001@gmail.com>
2025-08-13 20:34:53 +0630

    update automated testing

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,18 +1,22 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+console.log('Testing format currency:')
+console.log('Work with normal')
 if (formatCurrency(2089) === '20.89') {
     console.log("Passed")
 } else {
     console.log("Failed")
 }
 
+console.log('Work with round number')
 if (formatCurrency(2000.5) === "20.01") {
     console.log('Passed')
 } else {
     console.log('Failed')
 }
 
-if (formatCurrency(2456.678) === "24.57") {
+console.log('Work with 0')
+if (formatCurrency(0) === "0.00") {
     console.log('Passed')
 } else {
     console.log('Failed')
