e0674abd24d777ec1d9a872654224e6bdb0e10b4
lwinhtoosan <lwinhtoosan2001@gmail.com>
2025-08-14 16:27:41 +0630

    rename tests-jasmine to tests

--- tests/tests/moneyTest.js
+++ tests/tests/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 console.log('Testing format currency:')
 console.log('Work with normal')
